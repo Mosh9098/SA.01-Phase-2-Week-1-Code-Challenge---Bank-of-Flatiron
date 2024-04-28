83d84720bed7c757e6835481abe2989fdabd0152
mosh9098 <mosh9098@gmail.com>
2024-04-29 00:53:51 +0300

    complete

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import TransactionsList from "./TransactionList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransaction";
